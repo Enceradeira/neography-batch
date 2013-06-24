4f4a4ef38cf2c3d4d7eaeee958ea6f7b0cc6a899
Jorg Jenni <jorg.jenni@jennius.co.uk>
2013-06-24 10:47:32 +0100

    readability of specs improved

--- spec/lib/batch_reference_spec.rb
+++ spec/lib/batch_reference_spec.rb
@@ -1,31 +1,51 @@
-require "spec_helper"
+require 'spec_helper'
 
 module Neography
   module Composable
     describe BatchReference do
-      describe "==" do
-        specify do
-          ref1 = BatchReference.new([:create, {"id" => 1}])
-          ref2 = BatchReference.new([:create, {"id" => 1}])
-          (ref1==ref2).should be_false
-        end
-        specify do
-          command = [:create, {"id" => 1}]
-          ref1 = BatchReference.new(command)
-          ref2 = BatchReference.new(command)
-          (ref1==ref2).should be_true
-        end
-        specify do
-          command = [:create, {"id" => 1}]
-          ref1 = BatchReference.new(command)
-          ref1.after_submit do
-            puts "Do this"
+      describe '==' do
+        context 'different command-instances' do
+          let(:command1) { [:create, {"id" => 1}] }
+          let(:command2) { [:create, {"id" => 1}] }
+          let(:ref1) { BatchReference.new(command1) }
+          let(:ref2) { BatchReference.new(command2) }
+
+          it 'is not equal' do
+            expect(ref1).not_to eq(ref2)
           end
-          ref2 = BatchReference.new(command)
-          ref2.after_submit do
-            puts "Do something else"
+        end
+        context 'same command-instances' do
+          let(:command) { [:create, {"id" => 1}] }
+          let(:ref1) { BatchReference.new(command) }
+          let(:ref2) { BatchReference.new(command) }
+
+          it 'is equal' do
+            expect(ref1).to eq(ref2)
           end
-          (ref1==ref2).should be_false
+
+          context 'same after_submit block' do
+            let(:block) { Proc.new { | |} }
+            before do
+              ref1.after_submit(&block)
+              ref2.after_submit(&block)
+            end
+
+            it 'is equal' do
+              expect(ref1).to eq(ref2)
+            end
+          end
+          context 'different after_submit block' do
+            let(:block1) { Proc.new { | |} }
+            let(:block2) { Proc.new { | |} }
+            before do
+              ref1.after_submit(&block1)
+              ref2.after_submit(&block2)
+            end
+
+            it 'is equal' do
+              expect(ref1).not_to eq(ref2)
+            end
+          end
         end
       end
     end
